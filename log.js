bfc3ca087120d176b9e1cdafc2794bd0a1586469
shermam.miranda <shermam.miranda@hotmail.com>
2018-01-31 14:50:57 -0200

    Ajuste para logar a mensagem do erro

--- log.js
+++ log.js
@@ -1,6 +1,11 @@
 let count = 1;
 
 export function log(msg) {
+
+    if (msg.message) {
+        msg = msg.message;
+    }
+
     document.querySelector('#message')
         .innerHTML = count++ + ' - ' + JSON.stringify(msg) + '<br>';
 }
